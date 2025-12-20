8c58bada5de5b5d09dee591d6533e3486b36a50f
saiheroforking <you@example.com>
2025-12-20 15:54:33 +0530

    day-7 commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext ,useEffect} from "react";
+import React, { useState, createContext, useEffect } from "react";
 import ChildComponent from "./childComponent";
 import ErrorBoundary from "./ErrorBoundary";
 import "./App.css";
@@ -7,33 +7,33 @@ export const ThemeContext = createContext();
 
 function App() {
   const [theme, setTheme] = useState("light");
-  const [data,setData] = useState();
+  const [data, setData] = useState();
 
-  const toggle = () => {
+  const toggle = () =>
     setTheme(prev => (prev === "dark" ? "light" : "dark"));
-  };
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/python/")
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch(error => console.error("Error fetching data:", error));
-  }, []);
 
-  return (
-    <div className={`app ${theme}`}>
-      <ThemeContext.Provider value={theme}>
+useEffect(() => {
+  fetch("http://127.0.0.1:8000/api/python/")
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(error => console.error("Error fetching data:", error));
+}, []);
 
-        <h3>Theme: {theme}</h3>
-        <button onClick={toggle}>Change Theme</button>
-        <h1>Fetched Data</h1>
-        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
-        <ErrorBoundary>
-          <ChildComponent />
-        </ErrorBoundary>
+return (
+  <div className={`app ${theme}`}>
+    <ThemeContext.Provider value={theme}>
 
-      </ThemeContext.Provider>
-    </div>
-  );
+      <h3>Theme: {theme}</h3>
+      <button onClick={toggle}>Change Theme</button>
+      <h1>Fetched Data</h1>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
+      <ErrorBoundary>
+        <ChildComponent />
+      </ErrorBoundary>
+
+    </ThemeContext.Provider>
+  </div>
+);
 }
 
 export default App;
